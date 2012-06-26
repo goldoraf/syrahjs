e2accc2b5bcd98cb7e2244f7788af1f54a81dc63
Raphaël Rougeron <goldoraf@gmail.com>
2012-06-26 11:19:12 +0200

    Removed useless marshalling test

--- tests/src/json_marshaller_test.js
+++ tests/src/json_marshaller_test.js
@@ -16,30 +16,6 @@ module("JSON Marshaller tests", {
 	}
 });
 
-/*test("Simple object marshalling", function() {
-	var contact = Foo.Contact.create({ firstname: 'John', lastname: 'Doe' });
-	deepEqual(marshaller.marshall(contact), { firstname: 'John', lastname: 'Doe' });
-});
-
-test("Simple object with 1-n association unmarshalling", function() {
-	var json = { 
-		firstname: 'John', 
-		lastname: 'Doe',
-		phones: [
-            { type: "mobile", number: "+12345678" },
-            { type: "mobile", number: "+87654321" },
-		]
-	};
-	var loadedContact = marshaller.unmarshall(json, Foo.Contact.create());
-	
-	equal(loadedContact.get('firstname'), 'John');
-	ok(loadedContact.get('phones') instanceof Array);
-	ok(loadedContact.get('phones').objectAt(0) instanceof Foo.Phone);
-	ok(loadedContact.get('phones').objectAt(1) instanceof Foo.Phone);
-	equal(loadedContact.get('phones').objectAt(0).get('number'), '+12345678');
-	equal(loadedContact.get('phones').objectAt(1).get('number'), '+87654321');
-});*/
-
 test("Simple model marshalling", function() {
     var Contact = Syrah.Model.define({
         name: String
